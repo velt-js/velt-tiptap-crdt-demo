d48c753e6aea9cda8ff91e809b3d7c96766cd96f
vivekk-snippyly <vivek@snippyly.com>
2025-07-02 18:43:59 +0530

    Add VeltPresence component for user presence indication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { VeltProvider } from '@veltdev/react'
+import { VeltPresence, VeltProvider } from '@veltdev/react'
 import './App.css'
 import TipTap from './components/TipTap'
 import VeltCollaboration from './velt-components/VeltCollaboration'
@@ -12,6 +12,7 @@ function App() {
           <header className="app-header">
             <h1 className="app-title">Velt Collaborative Tiptap Editor</h1>
             <div className="login-section">
+              <VeltPresence />
               <VeltCollaboration />
             </div>
           </header>
